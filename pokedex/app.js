ea1c3608893612e979c458dbc5018a09a7d3d435
masonsteeger94 <masonsteeger@gmail.com>
2020-08-29 17:07:52 -0500

    got my search bar to grab data from API

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,1 +1,16 @@
-console.log($);
+
+
+$(() => {
+  $('form').on('submit' , event => {
+    event.preventDefault();
+    const userInput = ($('#search-bar').val());
+    $('form').trigger('reset')
+    $.ajax({
+      url: 'https://pokeapi.co/api/v2/pokemon/'+userInput
+    }).then(
+      data => {
+        console.log(data);
+      }
+    )
+  })
+})
