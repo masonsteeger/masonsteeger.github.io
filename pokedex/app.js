75e5293a8fba0bd927c2a371e2a118965b0e3140
masonsteeger94 <masonsteeger@gmail.com>
2020-08-29 20:51:37 -0500

    searching will now append types and assign a color to them

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,7 +1,6 @@
 
 
 $(() => {
-
   //When a search is performed
   $('form').on('submit' , event => {
     event.preventDefault();
@@ -9,12 +8,12 @@ $(() => {
     $('.pokemon-info').children().remove();
     //creating divs for serched pokemon stats/attr
     let $nameDiv = $('<div>').attr('id','name').text('Name: ');
-    let $idDiv = $('<div>').attr('id','id').text('National Index: ')
-    let $typeDiv = $('<div>').attr('id','type').text('Type: ')
+    let $idDiv = $('<div>').attr('id','id').text('Nat. Index: ');
+    let $typeDiv = $('<div>').attr('id','type').text('Type:  ');
     //defining the user's desired pokemon search
     let userInput = ($('#search-bar').val().toLowerCase());
     if (userInput === ''){
-      alert('Please Enter a Pokemon Name or National Index Number')
+      alert('Please Enter a Pokemon Name or National Index Number');
       return
     }
     $.ajax({
@@ -22,16 +21,63 @@ $(() => {
       url: 'https://pokeapi.co/api/v2/pokemon/'+userInput
     }).then(
       data => {
-        $('.pokemon-info').append($nameDiv)
-        $('.pokemon-info').append($idDiv)
-        let $name = (data.name)
-        let $id = ('#'+data.id)
-        $('#name').append($name).css('text-transform', 'capitalize');
-        $('#id').append($id)
+        //appending info to main div
+        $('.pokemon-info').append($nameDiv).append($idDiv).append($typeDiv)
+        //grabbing info from API
+        let $name = (data.name);
+        let $id = ('#'+data.id);
+        //going through a loop to append types and assign type colors
+        for(i = 0; i < data.types.length; i++){
+          let $type = (data.types[i].type.name);
+          let $typeColorDiv = $('<div>').addClass('type')
+          console.log($type);
+          if($type === 'normal'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(168,168,119)')
+          }else if($type === 'fire'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(232,128,48)')
+          }else if($type === 'fighting'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(192,48,40)')
+          }else if($type === 'water'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(104,144,240)')
+          }else if($type === 'flying'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(168,144,240)')
+          }else if($type === 'grass'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(120,200,80)')
+          }else if($type === 'poison'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(160,64,160)')
+          }else if($type === 'electric'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(246,208,48)')
+          }else if($type === 'ground'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(224,192,104)')
+          }else if($type === 'psychic'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(226,87,137)')
+          }else if($type === 'rock'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(184,160,56)')
+          }else if($type === 'ice'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(152,216,216)')
+          }else if($type === 'bug'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(168,184,32)')
+          }else if($type === 'dragon'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(113,62,248)')
+          }else if($type === 'ghost'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(112,88,152)')
+          }else if($type === 'dark'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(112,88,72)')
+          }else if($type === 'steel'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(184,184,208)')
+          }else if($type === 'fairy'){
+            $typeColorDiv.append($type).css('background-color', 'rgb(234,153,172)')
+          }
+          $typeDiv.append($typeColorDiv)
+          $typeDiv.append(' ');
+        }
+        //appending name/id to main div
+        $nameDiv.append($name).css('text-transform', 'capitalize');
+        $idDiv.append($id);
         console.log(data);
-        console.log(data.types[0].type.name);
       }
     )
-    $('form').trigger('reset')
+    //resetting the search bar to be empty
+    $('form').trigger('reset');
   })
 })
