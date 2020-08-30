d8581e686bc73fc0f32b8e5cbcb78762707e707a
masonsteeger94 <masonsteeger@gmail.com>
2020-08-29 19:03:59 -0500

    searching will now append pokemon name and index#

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,16 +1,37 @@
 
 
 $(() => {
+
+  //When a search is performed
   $('form').on('submit' , event => {
     event.preventDefault();
-    const userInput = ($('#search-bar').val());
-    $('form').trigger('reset')
+    //removing previous children from container div
+    $('.pokemon-info').children().remove();
+    //creating divs for serched pokemon stats/attr
+    let $nameDiv = $('<div>').attr('id','name').text('Name: ');
+    let $idDiv = $('<div>').attr('id','id').text('National Index: ')
+    let $typeDiv = $('<div>').attr('id','type').text('Type: ')
+    //defining the user's desired pokemon search
+    let userInput = ($('#search-bar').val().toLowerCase());
+    if (userInput === ''){
+      alert('Please Enter a Pokemon Name or National Index Number')
+      return
+    }
     $.ajax({
+      //pulling data from the API via userInput
       url: 'https://pokeapi.co/api/v2/pokemon/'+userInput
     }).then(
       data => {
+        $('.pokemon-info').append($nameDiv)
+        $('.pokemon-info').append($idDiv)
+        let $name = (data.name)
+        let $id = ('#'+data.id)
+        $('#name').append($name).css('text-transform', 'capitalize');
+        $('#id').append($id)
         console.log(data);
+        console.log(data.types[0].type.name);
       }
     )
+    $('form').trigger('reset')
   })
 })
