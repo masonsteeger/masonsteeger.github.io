57e547a8cf87fb464d5af2fe4caf04c0c0cc4811
masonsteeger94 <masonsteeger@gmail.com>
2020-10-30 17:35:22 -0500

    added more styling and animations

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,59 +1,96 @@
-console.log($);
+
 $(() => {
   // MENU TOGGLE
   $('#menu-toggle').on('click', () => {
     $('#hidden-nav').toggle(500);
   })
 
-
+  console.log();
 
   // PAGES TOGGLE
-  const toggleDivs = ($divID) => {
+
+  const $toggleDivs = ($divID) => {
     $('#home-container, #project-container, #bio-container, #resume-container').addClass("hide")
     $('#home-container,#project-container, #bio-container, #resume-container').removeClass("show")
     $($divID).addClass("show")
     $($divID).removeClass("hide")
   }
 
+
+  const fadeDivs = ($divID) => {
+    switch(true){
+      case ($('#project-container').attr('class') === 'show'):
+        $(`#project-container`).fadeOut(500, () => {
+        $(`${$divID}`).fadeIn(500)
+        });
+      case ($('#bio-container').attr('class') === 'show'):
+        $(`#bio-container`).fadeOut(500, () => {
+        $(`${$divID}`).fadeIn(500)
+        });
+      case ($('#resume-container').attr('class') === 'show'):
+        $(`#resume-container`).fadeOut(500, () => {
+        $(`${$divID}`).fadeIn(500)
+        });
+    }
+  }
+
+
   $('#home-projects').on('click', () => {
-    toggleDivs('#project-container')
-    $('#nav-bar').removeClass("hide")
+    $('#home-container').fadeOut(500, () => {
+      $('#project-container').fadeIn(500)
+      $('#nav-bar').fadeIn(500, () => {
+        $toggleDivs('#project-container')
+      })
+    });
     $('#title').html("PROJECTS")
   })
+
   $('#home-bio').on('click', () => {
-    toggleDivs('#bio-container')
-    $('#nav-bar').removeClass("hide")
+    $('#home-container').fadeOut(500, () => {
+      $('#bio-container').fadeIn(500)
+      $('#nav-bar').fadeIn(500, () => {
+        $toggleDivs('#bio-container')
+      })
+    });
     $('#title').html("BIO")
   })
+
   $('#home-resume').on('click', () => {
-    toggleDivs('#resume-container')
-    $('#nav-bar').removeClass("hide")
+    $('#home-container').fadeOut(500, () => {
+      $('#resume-container').fadeIn(500)
+      $('#nav-bar').fadeIn(500, () => {
+        $toggleDivs('#resume-container')
+      })
+    });
     $('#title').html("RESUME")
   })
+
+
   $('#nav-home').on('click', () => {
-    toggleDivs('#home-container')
-    $('#nav-bar').addClass("hide")
-    $('#hidden-nav').toggle(0);
+    $('#hidden-nav').toggle(500, fadeDivs(`#home-container`));
+    $('#nav-bar').fadeOut()
+    $toggleDivs('#home-container')
   })
 
   $('#nav-projects').on('click', () => {
-    toggleDivs('#project-container')
-    $('#hidden-nav').toggle(1000);
+    $('#hidden-nav').toggle(500, fadeDivs(`#project-container`));
+    $toggleDivs('#project-container')
     $('#title').html("PROJECTS")
   })
   $('#nav-bio').on('click', () => {
-    toggleDivs('#bio-container')
-    $('#hidden-nav').toggle(1000);
+    $('#hidden-nav').toggle(500, fadeDivs(`#bio-container`));
+    $toggleDivs('#bio-container')
     $('#title').html("BIO")
   })
   $('#nav-resume').on('click', () => {
-    toggleDivs('#resume-container')
-    $('#hidden-nav').toggle(1000);
+    $('#hidden-nav').toggle(500, fadeDivs(`#resume-container`));
+    $toggleDivs('#resume-container')
     $('#title').html("RESUME")
   })
 
-  //LINKEDIN HOVER
 
+
+  //LINKEDIN HOVER
     $("#linkedin").hover(function(){
       $(this).attr("src", function(index, attr){
           return attr.replace("./icons/linkedin.png", "./icons/linkedin-hover.png");
@@ -63,6 +100,8 @@ $(() => {
           return attr.replace("./icons/linkedin-hover.png", "./icons/linkedin.png");
       });
   });
+
+
   //GITHUB HOVER
   $("#github").hover(function(){
     $(this).attr("src", function(index, attr){
